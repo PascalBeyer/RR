514533e3c99cfbe7b827feacf29a91492467a731
PascalBeyer <40343897+PascalBeyer@users.noreply.github.com>
2019-01-02 12:45:14 +0100

    Delete AnimationCreator.h

--- AnimationCreator.h
+++
@@ -1,9 +0,0 @@
-#ifndef RR_ANIMATIONCREATOR
-#define RR_ANIMATIONCREATOR
-
-void AnimationCreatorInit();
-//void AnimationCreatorUpdateAndRender(RenderCommands *renderComamds, Input *Input);
-
-
-#endif // !RR_ANIMATIONCREATOR
-
